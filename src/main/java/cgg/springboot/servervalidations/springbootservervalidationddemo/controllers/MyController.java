a6a7aa752bc174c67b88fd7936b88652e3216025
Farooq <fmd894717@gmail.com>
2023-09-05 15:45:31 +0530

    commit 05-09-2023

--- src/main/java/cgg/springboot/servervalidations/springbootservervalidationddemo/controllers/MyController.java
+++ src/main/java/cgg/springboot/servervalidations/springbootservervalidationddemo/controllers/MyController.java
@@ -1,8 +1,38 @@
 package cgg.springboot.servervalidations.springbootservervalidationddemo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import cgg.springboot.servervalidations.springbootservervalidationddemo.entities.LoginData;
+import jakarta.validation.Valid;
+
+
 
 @Controller
 public class MyController {
     
+    @GetMapping("/form")
+    public String openForm(Model m){
+
+        System.out.println("opening form");
+        m.addAttribute("loginData", new LoginData());
+        return "form";
+    }
+
+    //Handler for process form
+    @PostMapping("/process")
+    public String processForm(@Valid @ModelAttribute LoginData loginData,BindingResult result){
+        if(result.hasErrors()){
+            System.out.println(result);
+            return "form";
+        }
+        System.out.println(loginData);
+
+        //Data process
+        return  "success";
+    }
 }
--- src/main/java/cgg/springboot/servervalidations/springbootservervalidationddemo/entities/LoginData.java
+++ src/main/java/cgg/springboot/servervalidations/springbootservervalidationddemo/entities/LoginData.java
@@ -1,5 +1,58 @@
 package cgg.springboot.servervalidations.springbootservervalidationddemo.entities;
 
-public record LoginData(String userName,String email, boolean agreed) {
-    
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class LoginData{
+                         @NotBlank(message = "UserName cannot be empty!")
+                         @Size(min=3,max = 12,message = "UserName must be 3 to 12 characters")
+                        private String userName;
+                         @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", message = "Invalid email!")
+                         private String email;
+                        @AssertTrue(message = "Must agree Terms&Conditions")
+                        private boolean agreed= false;
+
+    public LoginData(){
+        //default constructor
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LoginData(
+            @NotBlank(message = "UserName cannot be empty!") @Size(min = 3, max = 12, message = "UserName must be 3 to 12 characters") String userName,
+            @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", message = "Invalid email!") String email,
+            @AssertTrue(message = "Must agree Terms&Conditions") boolean agreed) {
+        this.userName = userName;
+        this.email = email;
+        this.agreed = agreed;
+    }
+
+    public boolean isAgreed() {
+        return agreed;
+    }
+
+    public void setAgreed(boolean agreed) {
+        this.agreed = agreed;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginData [userName=" + userName + ", email=" + email + ", agreed=" + agreed + "]";
+    }
 }
